72c8d76c358d446ebddec4f9889b3cfa4bec10a1
Hassineen <167764197+Hassineen@users.noreply.github.com>
2024-12-17 19:18:17 +0100

    Modifications
    Last changes

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX with Gradle!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("JavaFX Gradle Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
